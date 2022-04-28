1a147bfddbe410f58bf56e1b9de55a8a2b555885
Tasio Victoria <tasiovictoria@gmail.com>
2022-04-28 13:19:06 -0400

    Replace cw2891 with pg721 off-chain metadata contract

--- contracts/cw2981-royalties/scripts/optimize.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker run --rm -v "$(pwd)":/code \
-	--platform linux/x86_64 \
-	--mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
-	--mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-	cosmwasm/rust-optimizer:0.12.6
