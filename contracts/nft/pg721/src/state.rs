60bee061fcf666a6f44c76cb64996306badee4d0
0xmuralik <krishna192reddy@gmail.com>
2023-06-16 15:15:33 +0530

    add migration status to state pg721

--- contracts/nft/pg721/src/state.rs
+++ contracts/nft/pg721/src/state.rs
@@ -1,4 +1,4 @@
-use cosmwasm_std::{Addr, Decimal};
+use cosmwasm_std::{Addr, Decimal, DepsMut, StdResult, Deps};
 use cw_storage_plus::Item;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
@@ -18,4 +18,23 @@ pub struct RoyaltyInfo {
     pub share: Decimal,
 }
 
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct MigrationStatus {
+    pub done: bool,
+}
+
+// marks migrations as done
+pub fn migration_done(deps: DepsMut) -> StdResult<()>{
+    let mut status=MIGRATION_STATUS.load(deps.storage)?;
+    status.done=true;
+    MIGRATION_STATUS.save(deps.storage, &status)
+}
+
+// returns true if migration is done
+pub fn migration_status(deps: Deps) -> bool {
+    let status = MIGRATION_STATUS.load(deps.storage).unwrap();
+    return status.done;
+}
+
 pub const COLLECTION_INFO: Item<CollectionInfo<RoyaltyInfo>> = Item::new("collection_info");
+pub const MIGRATION_STATUS: Item<MigrationStatus> = Item::new("migration_status");
